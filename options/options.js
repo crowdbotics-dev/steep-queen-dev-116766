9b271b6de008ee273f90394d7411c3a1cc2881f1
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 08:13:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-queen-dev-116766",
+  url: "https://steep-queen-dev-116766.botics.co",
+  api: "https://steep-queen-dev-116766.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
